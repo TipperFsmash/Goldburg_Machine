b06f500c43705002743b81f48c27c93199ebc9b1
TipperFsmash <fox.buxton@gmail.com>
2018-05-20 02:09:41 +0100

    Final

---
+++ Assets/BallNoises.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallNoises : MonoBehaviour {
+
+    public AudioSource Ball;
+    public AudioClip CollisionSound;
+    public AudioClip Collisionspin;
+
+    void Start()
+    {
+        Ball = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Target")
+        {
+            Ball.PlayOneShot(CollisionSound);
+        }
+
+        if (collision.gameObject.tag == "spin slow")
+        {
+            Ball.PlayOneShot(Collisionspin);
+        }
+    }
+
+}
---
+++ Assets/destroydiir.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroydiir : MonoBehaviour {
+
+    public AudioSource Door;
+    public AudioClip CollisionSound;
+
+
+    void Start()
+    {
+       
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Door")
+        {
+           Destroy (collision.gameObject);
+        }
+
+        if (collision.gameObject.tag == "Door")
+        {
+            Door.PlayOneShot(CollisionSound);
+        }
+
+    }
+
+}
